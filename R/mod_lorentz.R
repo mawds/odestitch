edb3c6517097fa66f4bc25dd5ad202f6ba26d0f1
David Mawdsley <david@tupledata.co.uk>
2024-11-05 15:23:12 +0000

    Allow finer grained parameter selection, reparametrise time period

--- R/mod_lorentz.R
+++ R/mod_lorentz.R
@@ -10,11 +10,11 @@
 mod_lorentz_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    sliderInput(ns("sigma"), "Sigma", value=10, min=1,max=20),
-    sliderInput(ns("beta"), "Beta", value=8/3, min=1, max=10),
-    sliderInput(ns("rho"), "Rho", value=28, min=1,max=50),
-    numericInput(ns("delta_t"), "delta t", value=1e-3),
-    numericInput(ns("steps"), "Steps", value=5e3)
+    sliderInput(ns("sigma"), "Sigma", value=10, min=1,max=20, step=0.1, round=0.1),
+    sliderInput(ns("beta"), "Beta", value=8/3, min=1, max=10, step=0.1, round=0.1),
+    sliderInput(ns("rho"), "Rho", value=28, min=1,max=50, step=0.1, round=0.1),
+    numericInput(ns("delta_t"), "delta t", value=1e-3, min=1e-5, max=0.1),
+    numericInput(ns("max_time"), "Maximium time", value=100, min=1, max=500)
     # TODO y[1:3]
   )
 }
@@ -31,8 +31,9 @@ mod_lorentz_server <- function(id){
               beta=input$beta,
               rho=input$rho)
       times=seq(from=0,
-                by=input$delta_t,
-                length.out=input$steps)
+                to=input$max_time,
+                by=input$delta_t)
+
 
       values <- solve_lorentz(parms=parms, times=times)
       return(values)
